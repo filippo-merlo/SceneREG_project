00bacc7e58ff27a58865dc7ba08d8f55cb854ee1
Filippo <filippo99merlo@gmail.com>
2024-07-02 16:07:58 +0200

    `chore: Update size_mean_path for object scene norms calculation`

--- code/dataset/utils.py
+++ code/dataset/utils.py
@@ -67,40 +67,6 @@ def add_in_bounds(x, y, max):
 
 # FIND OBJECT TO REPLACE 
 
-#%%
-# Load the map_coco2ade file
-with open('/Users/filippomerlo/Documents/GitHub/SceneREG_project/code/dataset/mappings/object_map_coco2ade.json', "r") as file:
-    map_coco2ade = json.load(file)
-
-# Load the object_map_ade2things file
-with open('/Users/filippomerlo/Documents/GitHub/SceneREG_project/code/dataset/mappings/object_map_ade2things.json', "r") as file:
-    map_ade2things = json.load(file)
-
-# Load the map SUN 2 ADE
-with open('/Users/filippomerlo/Documents/GitHub/SceneREG_project/code/dataset/mappings/sun2ade_map.json', "r") as file:
-    map_sun2ade = json.load(file)
-
-# Load ade names
-path = '/Users/filippomerlo/Desktop/Datasets/ADE20K_2021_17_01/index_ade20k.pkl'
-with open(path, 'rb') as f:
-    ade20k_index = pkl.load(f)
-ade20k_object_names = ade20k_index['objectnames']
-
-# Filter objects by remooving the ones that appears in one scene only
-with open('/Users/filippomerlo/Documents/GitHub/SceneREG_project/code/dataset/compute_scene_obj_similarity/tf_scores.pkl', 'rb') as f:
-    tf_scores = pkl.load(f)
-
-# Define a function to set each cell to 1 if it has a non-zero value
-def set_to_one(x):
-    return 1 if x != 0 else 0
-# Apply the function to each cell of the DataFrame
-tf_scores_ones = tf_scores.applymap(set_to_one)
-row_sums = tf_scores_ones.sum(axis=1)
-ones_id = [i for i, num in enumerate(row_sums) if num == 1]
-object_to_remove = [x for i, x in enumerate(ade20k_object_names) if i in ones_id]
-object_to_remove
-#%%
-
 def find_object_to_replace(target_object_name, scene_name):
     scene_name = map_sun2ade[scene_name.replace('/', '_')]
     # get the more similar in size with the less semantic relatedness to the scene
