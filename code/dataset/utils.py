1ecbc0a615b2e083b2aa2f68e398f5fb36d01315
Filippo <filippo99merlo@gmail.com>
2024-07-04 14:15:32 +0200

    `Refactor generate function to use more descriptive variable names and improve code readability`

--- code/dataset/utils.py
+++ code/dataset/utils.py
@@ -553,6 +553,7 @@ def generate(init_image, target_box, new_object):
 
     # Mask out the area defined by x, y, w, h
     mask[int(y):int(y_end), int(x):int(x_end)] = 1
+    print(new_object)
     prompt = f"a {new_object}, realistic, highly detailed, 8k"
     negative_prompt = "bad anatomy, deformed, ugly, disfigured"
     generated_image = pipeline(prompt=prompt,negative_prompt=negative_prompt, image=init_image, mask_image=mask, generator=generator).images[0]
