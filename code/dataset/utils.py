331fe7b0289d7f61c1918df80c98b9c1db571daf
Filippo <filippo99merlo@gmail.com>
2024-07-04 13:28:34 +0200

    `Refactor get_images_names function to remove unnecessary print statements and improve code efficiency`

--- code/dataset/utils.py
+++ code/dataset/utils.py
@@ -289,6 +289,7 @@ def get_images_names(substitutes_list):
     for folder_name in things_folder_names:
         folders_path = os.path.join(things_images_path, folder_name)
         images_paths = get_all_names(folders_path)
+        print(images_paths)
         for i_p in images_paths:
             things_obj_name = re.sub(r"\d+", "",i_p.split('/')[-2]).replace('_',' ')
             if folder_name == things_obj_name:
