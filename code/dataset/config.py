952ac35f959730b9e17fd052751ba840b69b5a5a
Filippo <filippo99merlo@gmail.com>
2024-07-08 13:24:05 +0200

    `Update torch_dtype in StableDiffusion3InpaintPipeline initialization`

--- code/dataset/config.py
+++ code/dataset/config.py
@@ -132,6 +132,6 @@ from diffusers.utils import load_image
 
 pipe = StableDiffusion3InpaintPipeline.from_pretrained(
     "stabilityai/stable-diffusion-3-medium-diffusers",
-    torch_dtype=torch.float16,
+    #torch_dtype=torch.float16,
     cache_dir=CACHE_DIR_SHARED
 ).to(device)
