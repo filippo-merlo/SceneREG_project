6b43a2621565a9651e0fca6ff7fb789f75c15783
Filippo <filippo99merlo@gmail.com>
2024-07-10 19:18:13 +0200

    `Refactor size distance calculation and guidance scale in generate_sd3`

--- code/dataset/utils.py
+++ code/dataset/utils.py
@@ -695,8 +695,8 @@ def generate_sd3(image, target_box, new_object):
         height=size,
         width=size,
         num_inference_steps=100,
-        guidance_scale=7.0,
-        strength=0.6,
+        guidance_scale=9.0,
+        strength=0.9,
     ).images
 
     return generated_image, mask_image
