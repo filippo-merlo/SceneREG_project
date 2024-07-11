a960bc89d67bfa4dfbf07de3e3d14050ab7ab11e
Filippo <filippo99merlo@gmail.com>
2024-07-11 15:55:27 +0200

    `Refactor generate_sd3 function and add high quality photography prompt`

--- code/dataset/utils.py
+++ code/dataset/utils.py
@@ -631,10 +631,12 @@ def generate_sd3(image, target_box, new_object):
         mask_image
     )
 
-    prompt = f"a {new_object}"
-
+    prompt = f"a high quality photography of a {new_object} taken with Canon EOS R3a"
+    negative_prompt = f"worst quality, normal quality, low quality, low res, blurry, text, watermark, logo, banner, extra digits, cropped, jpeg artifacts, signature, username, error, sketch ,duplicate, ugly, monochrome, horror, geometry, mutation, disgusting"
+    
     generated_image = pipe(
         prompt=prompt,
+        negative_prompt=negative_prompt,
         image=source,
         mask_image=mask,
         height=size,
