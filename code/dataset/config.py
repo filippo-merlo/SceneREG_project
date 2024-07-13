4a1c0629c03c7954039ea5df965d1d8ffbb23bdd
Filippo <filippo99merlo@gmail.com>
2024-07-13 14:11:51 +0200

    `Refactor dataset config to comment out unused code`

--- code/dataset/config.py
+++ code/dataset/config.py
@@ -126,11 +126,11 @@ from torchvision import transforms
 from pipeline_stable_diffusion_3_inpaint import StableDiffusion3InpaintPipeline
 from diffusers.utils import load_image
 
-pipe = StableDiffusion3InpaintPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-3-medium-diffusers",
-    torch_dtype=torch.float16,
-    cache_dir=CACHE_DIR_SHARED
-).to(device_gen)
+#pipe = StableDiffusion3InpaintPipeline.from_pretrained(
+#    "stabilityai/stable-diffusion-3-medium-diffusers",
+#    torch_dtype=torch.float16,
+#    cache_dir=CACHE_DIR_SHARED
+#).to(device_gen)
 
 ### Init model for UPSCALING 
 
