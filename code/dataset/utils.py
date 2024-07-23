1c7f3ba74940e2d9fdbe46efe16a1e92c1236821
Filippo <filippo99merlo@gmail.com>
2024-07-23 11:50:01 +0200

    Refactor generate_sd3 function in utils.py

--- code/dataset/utils.py
+++ code/dataset/utils.py
@@ -739,8 +739,8 @@ def generate_sd3(pipe, image, target_box, new_object, scene_category, prompt_obj
         height=size,
         width=size,
         num_inference_steps=50,
-        guidance_scale=9,
-        strength=0.8,
+        guidance_scale=5,
+        strength=0.7,
         padding_mask_crop = 0
     ).images
 
