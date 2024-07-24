cf91c6a13b2de7cd7634ef55989209ae64db5984
Filippo <filippo99merlo@gmail.com>
2024-07-24 17:08:39 +0200

    `Refactor exception handling in generate_new_image function`

--- code/dataset/utils.py
+++ code/dataset/utils.py
@@ -832,8 +832,8 @@ def generate_new_image(data, n):
                 #image = api_upscale_image_gradio(path_to_img, 2)
                 save_path = os.path.join(data_folder_path+'/generated_images', f'{scene_category.replace('/','_')}_{target.replace('/','_')}_{images_names[0].replace('/','_')}_replaced_{i}.jpg')
                 image.save(save_path)
-        except:
-            print('error 2')
+        except Exception as e:
+            print(e)
 
 
         
