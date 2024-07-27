18fa712973f502270f09cf0c563d0f7f510f1698
Filippo <filippo99merlo@gmail.com>
2024-07-28 00:21:24 +0200

    Refactor prompt generation and update prompt with high definition in generate_sd3_from_patch function

--- code/dataset/utils.py
+++ code/dataset/utils.py
@@ -864,8 +864,8 @@ def generate_sd3_from_patch(pipe, image, mask, bbox_in_mask, new_object, scene_c
     else:
         art = 'A'
 
-    prompt = f"(single object), (complete object), realistic, center of the image, accurate, high quality, correct perspective."
-    prompt_2 = f"(single object), (complete object), realistic, center of the image, accurate, high quality, correct perspective."
+    prompt = f"(single object), (complete object), realistic, center of the image, accurate, high quality, high definition, correct perspective."
+    prompt_2 = f"(single object), (complete object), realistic, center of the image, accurate, high quality, high definition, correct perspective."
     prompt_3 = f"{art} {new_object}. {prompt_obj_descr}"
 
     print(int(w), int(h))
