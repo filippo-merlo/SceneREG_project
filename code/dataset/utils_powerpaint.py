571ee6e4a5b00935374363452e231d773f22e99f
Filippo <filippo99merlo@gmail.com>
2024-07-28 21:38:52 +0200

    s

--- code/dataset/utils_powerpaint.py
+++ code/dataset/utils_powerpaint.py
@@ -591,7 +591,7 @@ def generate_new_images(data, n):
                 removal_prompt,
                 removal_negative_prompt,
             )
-            
+            print(dict_out)
             save_path = os.path.join(data_folder_path+'/generated_images', f'{scene_category.replace('/','_')}_{target.replace('/','_')}_{images_names[0].replace('/','_')}.jpg')
             dict_out[0].save(save_path)
 
