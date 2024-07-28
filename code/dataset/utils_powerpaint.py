1dd5614df3fe1f3df61747765b06e4e1251faee4
Filippo <filippo99merlo@gmail.com>
2024-07-28 20:46:52 +0200

    Update PowerPaint checkpoint version to ppt-v2

--- code/dataset/utils_powerpaint.py
+++ code/dataset/utils_powerpaint.py
@@ -549,7 +549,27 @@ def generate_new_images(data, n):
             objects_for_replacement_list = find_object_for_replacement(target, scene_category)
             images_names, images_paths = compare_imgs(cropped_target_only_image, objects_for_replacement_list)
             print(images_names)
-        
+            if images_names[0][0] in ['a','e','i','o','u']:
+                art = 'an'
+            else:
+                art = 'a'
+            prompt = f"{art} {images_names[0]}"
+            text_guided_prompt = prompt
+            text_guided_negative_prompt = ''
+            shape_guided_prompt = prompt
+            shape_guided_negative_prompt = ''
+            fitting_degree = 0.5 # 0-1
+            ddim_steps = 45 # 1-50
+            scale = 7.5 # 1-30
+            seed = 666 # 0-2147483647
+            task = "shape-guided"
+            vertical_expansion_ratio = 2 #1-4
+            horizontal_expansion_ratio = 2 #1-4
+            outpaint_prompt = ''
+            outpaint_negative_prompt = ''
+            removal_prompt = ''
+            removal_negative_prompt = ''
+            
             # Inpainting the target
             output = controller.infer(
                 input_image,
