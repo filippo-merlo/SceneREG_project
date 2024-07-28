9ee871cf4541cba0fb5637c0f6b77912d7922f7a
Filippo <filippo99merlo@gmail.com>
2024-07-28 22:03:13 +0200

    Refactor generate_new_images function in utils_powerpaint.py

--- code/dataset/generate.py
+++ code/dataset/generate.py
@@ -5,7 +5,7 @@ from dataset import *
 if __name__ == '__main__':
     dataset = Dataset(dataset_path = dataset_path)
     data = dataset.data
-    n = 1
+    n = 2
     generate_new_images(data, n)
 
 
