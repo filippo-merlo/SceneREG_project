5ff6171351e0a229b528cc7f54c3d165df7379ea
Filippo <filippo99merlo@gmail.com>
2024-07-30 14:58:36 +0200

    ds

--- code/dataset/powerPaint.py
+++ code/dataset/powerPaint.py
@@ -540,6 +540,4 @@ class PowerPaintController:
                 controlnet_conditioning_scale,
             )
         else:
-            return self.predict(
-                input_image, prompt, fitting_degree, ddim_steps, scale, seed, negative_prompt, task, None, None
-            )
+  
